9092838c854a7911411088ef7b421d5d8c1a46e6
AlyxMoon <allisterkmoon@gmail.com>
2021-06-04 08:36:13 -0700

    jot down some notes on coding languages

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+const main = () => {
+  
+}
+
+$(main) // modern replacement for $(document).ready(function () {})
+
+
+/*
+language choices
+- JavaScript
+  - defining trait: usable in web development, easy to start coding
+- Python
+  - straightforward syntax, commonly used, popular for math
+- C#
+  - DotNet language, big for coding in any microsoft-specific context
+- Java
+  - Useful for Android, lot of old web applets, portability, Minecraft
+- Swift
+  - Used for Apple
+- PHP
+  - Commonly used for servers, WordPress
+
+*/
